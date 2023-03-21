e75478aff703dae723d19d67d98bc0503455f62b
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-03-21 13:46:22 +0900

    design : AddToDoSection 컴포넌트 스타일링 구현

--- src/lib/store/TodoStore.ts
+++ src/lib/store/TodoStore.ts
@@ -2,7 +2,9 @@ import { v4 as uuidv4 } from "uuid";
 import { writable } from "svelte/store";
 import { browser } from "$app/environment";
 
-const data = browser ? JSON.parse(localStorage.getItem("string") || "") : [];
+const data = browser
+	? JSON.parse(window.localStorage.getItem("string") || "")
+	: [];
 
 export const todos = writable(data);
 
@@ -15,6 +17,7 @@ todos.subscribe((value) => {
 
 export const addTodo = () => {
 	todos.update((currentTodos) => {
+		console.log(currentTodos);
 		return [...currentTodos, { id: uuidv4(), text: "", complete: false }];
 	});
 };
